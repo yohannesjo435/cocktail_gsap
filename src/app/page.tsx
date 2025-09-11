511ec8486b12e0270d69b393499a11919dd30697
yohannes <yohannesgoat@gmail.com>
2025-09-10 21:24:29 -0300

    feat: art section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,18 @@ import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import Cocktails from "@/app/components/Cocktails";
 import About from "@/app/components/About";
+import Art from "@/app/components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
   return (
-    <main>
+    <main className="pb-52">
       <Navbar />
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 }
